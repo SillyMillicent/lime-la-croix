40480781276bffdab56c6f1e66f380df1e069f0a
Silly Millie <adhdsillymillie@gmail.com>
2024-01-26 20:45:48 -0500

    Quartz sync: Jan 26, 2024, 8:45 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -12,7 +12,7 @@ export default (() => {
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
-    const iconPath = joinSegments(baseDir, "https://sillymillie.com/")
+    const iconPath = joinSegments(baseDir, "https://i.imgur.com/f11AfHS.png")
     const ogImagePath = `https://i.imgur.com/f11AfHS.png`
 
     return (
